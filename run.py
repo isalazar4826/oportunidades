d854692597afb2991432dab5301ed0c35b0057f6
isalazar4826 <31516046+isalazar4826@users.noreply.github.com>
2025-01-08 18:24:09 -0600

    Add files via upload

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    user_type = db.Column(db.String(10), nullable=False, default='USER')
 
 # Modelo de Oportunidad
 class Opportunity(db.Model):
@@ -40,6 +41,43 @@ class Opportunity(db.Model):
 @app.route('/')
 def home():
     return redirect(url_for('login'))
+
+@app.route('/create_admin', methods=['GET', 'POST'])
+def create_admin():
+    if request.method == 'POST':
+        username = request.form['username']
+        if User.query.filter_by(username=username).first():
+            return render_template('create_admin.html', error="Error: El nombre de usuario ya existe.")
+
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password, user_type='ADMIN')
+        db.session.add(new_user)
+        db.session.commit()
+        return "Admin user created successfully!"
+    return render_template('create_admin.html')
+
+@app.route('/admin')
+def admin():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    user = User.query.get(session['user_id'])
+    
+    # Verifica si el usuario tiene permisos de administrador
+    if user.user_type != 'ADMIN':
+        return "Access denied!"
+
+    opportunities = Opportunity.query.all()
+    
+    # Obtener los usuarios para cada oportunidad
+    for opportunity in opportunities:
+        opportunity.user = User.query.get(opportunity.user_id)
+    
+    return render_template('admin.html', opportunities=opportunities)
+
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -102,15 +140,30 @@ def ver_oportunidades():
     if 'user_id' not in session:
         return redirect(url_for('login'))
 
-    user_id = session['user_id']
-    oportunidades = Opportunity.query.filter_by(user_id=user_id).all()
+    user = db.session.get(User, session['user_id'])
+    
+    if user.user_type == 'ADMIN':
+        opportunities = Opportunity.query.all()
+        
+        # Obtener los usuarios para cada oportunidad
+        for opportunity in opportunities:
+            opportunity.user = User.query.get(opportunity.user_id)
+            # Convertir la fecha de creación a la zona horaria de CDMX
+            if opportunity.fecha_creacion:
+                cdmx_tz = pytz.timezone('America/Mexico_City')
+                opportunity.fecha_creacion = opportunity.fecha_creacion.replace(tzinfo=pytz.utc).astimezone(cdmx_tz)
+        
+        return render_template('admin.html', opportunities=opportunities)
+    else:
+        oportunidades = Opportunity.query.filter_by(user_id=user.id).all()
 
-    cdmx_tz = pytz.timezone('America/Mexico_City')
-    for oportunidad in oportunidades:
-        if oportunidad.fecha_creacion:
-            oportunidad.fecha_creacion = oportunidad.fecha_creacion.replace(tzinfo=pytz.utc).astimezone(cdmx_tz)
+        cdmx_tz = pytz.timezone('America/Mexico_City')
+        for oportunidad in oportunidades:
+            if oportunidad.fecha_creacion:
+                oportunidad.fecha_creacion = oportunidad.fecha_creacion.replace(tzinfo=pytz.utc).astimezone(cdmx_tz)
+        
+        return render_template('ver_oportunidades.html', oportunidades=oportunidades)
 
-    return render_template('ver_oportunidades.html', oportunidades=oportunidades)
 
 @app.route('/editar/<int:id>', methods=['GET', 'POST'])
 def editar_oportunidad(id):
@@ -133,22 +186,31 @@ def exportar_oportunidades():
         return redirect(url_for('login'))
 
     user_id = session['user_id']
-    oportunidades = Opportunity.query.filter_by(user_id=user_id).all()
+    user = User.query.get(user_id)
+
+    if user.user_type == 'ADMIN':
+        oportunidades = Opportunity.query.all()
+    else:
+        oportunidades = Opportunity.query.filter_by(user_id=user_id).all()
 
     output = StringIO()
     writer = csv.writer(output)
 
-    writer.writerow([ 
-        'Fecha Creación', 'ID', 'Nombre', 'Cliente', 'Vendedor', 'Producto',
-        'Fecha Solicitud', 'Fecha Entrega', 'Estatus Preventa', 'Descripción del Estatus Comercial', 
-        'Comentarios', 'DEAL Id'
+    # Cambiar el orden de las columnas
+    writer.writerow([
+        'ID', 'Fecha Creacion', 'Nombre Preventa', 'Nombre Oportunidad', 'Deal ID', 'Cliente', 'Vendedor', 'Producto',
+        'Fecha Solicitud', 'Fecha Entrega', 'Estatus Preventa', 'Estatus Comercial', 'Comentarios'
     ])
 
     for oportunidad in oportunidades:
-        writer.writerow([ 
-            oportunidad.fecha_creacion.strftime('%Y-%m-%d %H:%M:%S'),
+        nombre_preventa = User.query.get(oportunidad.user_id).username if user.user_type == 'ADMIN' else ''
+
+        writer.writerow([
             oportunidad.id,
+            oportunidad.fecha_creacion.strftime('%Y-%m-%d %H:%M:%S'),
+            nombre_preventa,
             oportunidad.nombre,
+            oportunidad.deal_id,
             oportunidad.cliente,
             oportunidad.vendedor,
             oportunidad.producto,
@@ -156,17 +218,29 @@ def exportar_oportunidades():
             oportunidad.fecha_entrega.strftime('%Y-%m-%d'),
             oportunidad.estatus,
             oportunidad.descripcion_estatus,
-            oportunidad.comentarios,
-            oportunidad.deal_id
+            oportunidad.comentarios
         ])
 
     output.seek(0)
 
+    fecha_hora = datetime.now().strftime('%d-%m-%Y_%H-%M')
+    nombre_archivo = f'Oportunidades2025_{fecha_hora}.csv'
+
     return Response(
         output.getvalue(),
         mimetype='text/csv',
-        headers={"Content-Disposition": "attachment;filename=oportunidades.csv"}
+        headers={"Content-Disposition": f"attachment;filename={nombre_archivo}"}
     )
+
+@app.route('/detalles/<int:id>')
+def detalles_oportunidad(id):
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    oportunidad = Opportunity.query.get_or_404(id)
+
+    return render_template('detalles_oportunidad.html', oportunidad=oportunidad)
+
 
 @app.route('/logout', methods=['POST'])
 def logout():
@@ -180,14 +254,19 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-        if not User.query.filter_by(username="testuser").first():
-            testuser = User(username="ivan", password=generate_password_hash("ivan123"))
+        if not User.query.filter_by(username="ivan").first():
+            testuser = User(username="ivan", password=generate_password_hash("ivan123", method='pbkdf2:sha256'), user_type='ADMIN')
             db.session.add(testuser)
             db.session.commit()
 
         if not User.query.filter_by(username="userfake").first():
-            userfake = User(username="userfake", password=generate_password_hash("fakepassword"))
+            userfake = User(username="userfake", password=generate_password_hash("fakepassword", method='pbkdf2:sha256'))
             db.session.add(userfake)
+            db.session.commit()
+        
+        if not User.query.filter_by(username="Juan Perez").first():
+            juanperez = User(username="Juan Perez", password=generate_password_hash("juanperez123", method='pbkdf2:sha256'))
+            db.session.add(juanperez)
             db.session.commit()
 
     app.run(debug=True)
